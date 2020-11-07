aeaf52f2dad354d0da1103f6949a11ef90124dbd
Krista <krista.coding@gmail.com>
2020-11-07 14:19:00 -0500

    udpate saved route

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 var express = require("express");
 var path = require("path");
 var fs = require("fs");
+const { json } = require("body-parser");
 
 // Sets up the Express App
 var app = express();
@@ -11,12 +12,21 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Serving static files in Express
+app.use(express.static('public'));
+
+var allNotes; 
+
 // Reading database to create notes variable 
 fs.readFile("db/db.json", "utf8", (err,data) => {
     if (err) throw err;
-    var allNotes = JSON.parse(data); 
-    return allNotes;
+    allNotes = JSON.parse(data); 
 })
+
+function updateDB(){
+fs.writeFile("db/db.json", JSON.stringify(allNotes), (err, data) => {
+    if (err) throw err;
+})}
 
 // Routes
 app.get("/notes", function(req, res) {
@@ -33,9 +43,10 @@ app.get("/api/notes", function(req, res){
 
 // API Routes
 app.post("/api/notes", function (req, res) {
-    var newNote = req.body;
+    var newNote = req.body; 
     allNotes.push(newNote); 
-    res.json(newNote);
+    updateDB();
+    res.json(newNote); //sending back to the frontend the json object, never gets commited to a file. 
     return console.log("New Note Added")
 })
 
